d2fccbf229659837df453cddb958e9d8ccdc63c7
szkj-web-he <83932083+szkj-web-he@users.noreply.github.com>
2022-06-23 11:45:44 +0800

    关于环境变量的展示

---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+import ReactDOM from "react-dom";
+
+let command = "";
+switch (process.env.NODE_ENV) {
+    case "development":
+        command = "npm run start";
+        break;
+    case "production":
+        command = "npm run build";
+        break;
+    case "test":
+        command = "npm run build-dev";
+        break;
+}
+
+ReactDOM.render(
+    <div
+        style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            backgroundColor: "#000",
+            color: "yellow",
+            padding: "15px 20px",
+            zIndex: 200,
+        }}
+    >
+        指令: {command}
+        <br />
+        <br />
+        process.env.NODE_ENV:{process.env.NODE_ENV}
+    </div>,
+    document.getElementById("root"),
+);
